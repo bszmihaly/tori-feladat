871d5fc52e3cdd73794734a95e402fe11a8ac695
bszmihaly <58901704+bszmihaly@users.noreply.github.com>
2021-06-03 08:42:22 +0200

    jasonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn help me

--- data/jason.js
+++ data/jason.js
@@ -1,3 +1,30 @@
 var data = {
-  
+  "1700":{
+    "0":{
+      "questionType":"embed", //embed, question
+      "embedSource":"https://link",
+      "answerType":"pickOne", //pickOne, typeAnswer
+      "answers": {
+        "0":"Urgot",
+        "1": "Turgot",
+        "2": "Russzó"
+      }
+    },
+    "1":{
+      "questionType":"question", //embed, question
+      "question": "Kicsoda fogja megírni ezeket a kérdéseket?",
+      "answerType":"pickOne", //pickOne, typeAnswer
+      "answers": {
+        "0":"Balázs",
+        "1": "Marci",
+        "2": "Máté"
+      }
+    },
+    "2":{
+     "questionType":"question", //embed, question
+      "question": "Kicsoda fogja megírni ezeket a kérdéseket?",
+      "answerType":"typeAnswer", //pickOne, typeAnswer
+      "correctAnswers":["Balázs","Marci", "Máté"] 
+    }
+  }
 }
