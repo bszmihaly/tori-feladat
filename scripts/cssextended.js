7ec4e65a9239c0a06febb5dd91edc4a0fe1a3b4e
bszmihaly <58901704+bszmihaly@users.noreply.github.com>
2021-06-06 21:15:12 +0200

    gg

--- scripts/cssextended.js
+++ scripts/cssextended.js
@@ -2,6 +2,7 @@ function StartTheHappenings(){
     var r = document.querySelector(':root');
 
     var inputboi = document.getElementById("input");
+    var inputbutton = document.getElementById("inputButton");
     var recolorme = document.getElementById("answerField");
     var bgcolor = "#aa6f34"
 
@@ -9,5 +10,8 @@ function StartTheHappenings(){
         recolorme.style.backgroundColor="#332e2e"
     };
     inputboi.addEventListener("focusout", () => {recolorme.style.backgroundColor=bgcolor}, true);
+
+    inputbutton.addEventListener("pointerenter", () => {recolorme.style.backgroundColor="#332e2e"}, true);
+    inputbutton.addEventListener("pointerout", () => {recolorme.style.backgroundColor=bgcolor}, true);
     //r.setAttribute('--bgcol', "#332e2e");
 }
