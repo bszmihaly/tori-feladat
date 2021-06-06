9d8616ba5d0fbe03798dab8500f71c4b11b6f1a6
bszmihaly <58901704+bszmihaly@users.noreply.github.com>
2021-06-06 21:18:19 +0200

    fix tiny bug

--- scripts/cssextended.js
+++ scripts/cssextended.js
@@ -3,15 +3,20 @@ function StartTheHappenings(){
 
     var inputboi = document.getElementById("input");
     var inputbutton = document.getElementById("inputButton");
+
+    var inputfocused =false;
+
+
     var recolorme = document.getElementById("answerField");
     var bgcolor = "#aa6f34"
 
     inputboi.onfocus= function(){
-        recolorme.style.backgroundColor="#332e2e"
+        inputfocused = true;
+        recolorme.style.backgroundColor="#332e2e";
     };
-    inputboi.addEventListener("focusout", () => {recolorme.style.backgroundColor=bgcolor}, true);
+    inputboi.addEventListener("focusout", () => {recolorme.style.backgroundColor=bgcolor, inputfocused = false}, true);
 
     inputbutton.addEventListener("pointerenter", () => {recolorme.style.backgroundColor="#332e2e"}, true);
-    inputbutton.addEventListener("pointerout", () => {recolorme.style.backgroundColor=bgcolor}, true);
+    inputbutton.addEventListener("pointerout", () => {if(!inputfocused){recolorme.style.backgroundColor=bgcolor}}, true);
     //r.setAttribute('--bgcol', "#332e2e");
 }
