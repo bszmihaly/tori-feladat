85aca0835380c85e6c5b70e3b999cd0f4c0465e3
bszmihaly <58901704+bszmihaly@users.noreply.github.com>
2021-06-08 20:51:35 +0200

    Starting digitalization of questions

--- data/jason.js
+++ data/jason.js
@@ -20,7 +20,73 @@ var data = {
       "correctAnswers":["balazs","marci", "mate"] 
     }]
   },
+  "0":{
+    "title": "Az Egyesült Államok - Kasza-Kovács Máté",
+    "data":[{
+      "questionType": 0,
+      "question": "Melyik hajón érkeztek Amerikába az első telepesek?",
+      "answerType": 0,
+      "answers": ["Mayflower", "Santa Maria", "Pinta"]
+    },
+    {
+      "questionType": 0,
+      "question": "Ki volt az Egyesült Államok első elnöke?",
+      "answerType": 0,
+      "answers": ["George Washington", "Thomas Jefferson", "Abraham Lincoln"]
+    },
+    {
+      "questionType": 0,
+      "question": "Ki volt az Amerikai Egyesült Államok harmadik elnöke?",
+      "answerType": 0,
+      "answers": ["Thomas Jefferson", "George Washington", "Theodore Roosevelt"]
+    }]
+  },
+  "1":{
+    "title": "A Függetlenségi nyilatkozat - Kasza-Kovács Máté",
+    "data":[{
+      "questionType": 0,
+      "question": "Hol volt az Amerikai függetlenségi háború döntő csatája?",
+      "answerType": 0,
+      "answers": ["Yorktown", "Gettysburg", "New York"]
+    },
+    {
+      "questionType": 0,
+      "question": "Hol volt a híres teadélután 1773-ban?",
+      "answerType": 0,
+      "answers": ["Boston", "Washington", "New York"]
+    },
+    {
+      "questionType": 0,
+      "question": "Mikor adták ki a Függetlenségi Nyilatkozatot?",
+      "answerType": 0,
+      "answers": ["1776 július 4.", "1773 december 16.", "1789 május 5."]
+    }]
+  },
   "end of all":{
     "correctWordsForAllPaths":["ASDFA", "EDGEEDFSE", "ASFREESDGTR"] //yes
   }
 }
+/*
+1.Melyik hajón érkeztek Amerikába az első telepesek?
+MAYFLOWER; Santa Maria, Pinta,
+2.Ki volt az Egyesült Államok első elnöke?
+GEORGE WASHINGTON; Thomas Jefferson, Abraham Lincoln,
+3.Mikor adták ki a Függetlenségi Nyilatkozatot?
+1776 JÚLIUS 4; 1773 december 16, 1789 május 5,
+4.Hol volt a híres teadélután 1773-ban?
+BOSTON; Washington, New York,
+5.Ki volt az Amerikai Egyesült Államok harmadik elnöke?
+THOMAS JEFFERSON, George Washington, Theodore Roosevelt,
+6.Hol volt az Amerikai függetlenségi háború döntő csatája?
+YORKTOWN; Gettysburg, New York,
+7.Kik nem támogatták az Egyesült Államok függetlenségét?
+8. ANGOLOK; franciák, holandok,
+9.Merre nem szabadott a telepeseknek terjeszkednie?
+ALLEGHENY-HEGYSÉGTŐL NYUGATRA; nagy tavaktól északra, Appalache hegységen túlra,
+10.Mikor adják ki az USA alkotmányát?
+1787(EZ); 1798, 1834, 
+11.Hány államból állt eredetileg az USA,
+13(EZ); 15, 23,
+12.Hol tartották 1774-ben az első kongresszust?
+PHILADELPHIA; Boston, Washington,
+*/
